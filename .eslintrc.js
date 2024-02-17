6a4a22f9c4741aeed7d49260479bf33f884979ef
dayvoid97 <sharma.kanchan3154@gmail.com>
2024-02-16 21:12:46 -0500

    sauri push
    push

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
-    amd: true,
-    node: true,
-    es6: true,
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
-  rules: {
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0,
-    'no-unused-vars': 0,
-    'react/no-unescaped-entities': 0,
+  extends: ['standard', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: ['react'],
+  rules: {}
 }
